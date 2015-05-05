82cf888b722ab0b8e390dfb1cfb9333d1e7f185c
Kevin Lee <kevinlee@feddev1.ddi.sheridanc.on.ca>
2015-05-05 11:29:57 -0400

    Initial commit

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,20 @@
+/**
+ * Created by kevinlee on 2015-05-04.
+ */
+/*var postsData = [ {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+},
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    }, {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];*/
+Template.postsList.helpers({
+    posts: function () {
+        return Posts.find({}, {sort: {submitted: -1}});
+    }
+});
