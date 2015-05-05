a394f12ea04b6a8a078e511b7cbd84171e9e4dfd
LinkXXI <kevinrobertlee@gmail.com>
2015-05-05 15:56:35 -0400

    Updates for the day

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,16 @@
+Template.postEdit.created = function(){
+    Session.set('postEditErrors', {});
+};
+
+Template.postEdit.helpers({
+   errorMessage: function (field) {
+       return Session.get('postEditErrors')[field];
+   },
+    errorClass: function (field){
+        return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postEdit.events({
     'submit form': function (e) {
         e.preventDefault();
@@ -5,11 +18,11 @@ Template.postEdit.events({
         var postProperties = {
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
-        }
+        };
         Posts.update(currentPostId, {$set: postProperties}, function (error) {
             if (error) {
                 // display the error to the user
-                alert(error.reason);
+                throwError(error.reason);
             } else {
                 Router.go('postPage', {_id: currentPostId});
             }
